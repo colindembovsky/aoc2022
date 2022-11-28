1c7f658ba1237fd99dd5e69ab267a83b8beeb17d
colindembovsky <colindembovsky@gmail.com>
2022-11-28 13:30:07 -0600

    Fix base

--- day00/part1.ts
+++ day00/part1.ts
@@ -1,12 +1,14 @@
 import * as fs from "fs";
 
+const ROOT_DIR="day00"
+
 // read in a file and print it out to the console
 function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
-let contents = readFile("test-input.txt");
-//let contents = readFile("input.txt");
+let contents = readFile(`${ROOT_DIR}/test-input.txt`);
+//let contents = readFile(`${ROOT_DIR}/input.txt`);
 //console.log(contents);
 
 console.log("==== PART 1 ====");
