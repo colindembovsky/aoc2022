f940f1a083187da03cbcec1518f5dc488172b798
colindembovsky <colindembovsky@gmail.com>
2022-12-03 23:42:50 -0600

    Solve day4

--- day04/part1.ts
+++ day04/part1.ts
@@ -7,10 +7,48 @@ function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
-console.log("==== PART 1 ====");
-let contents = readFile(`${ROOT_DIR}/test-input.txt`);
+class ElfRange {
+    public min: number;
+    public max: number;
+    
+    constructor(line: string) {
+        let [min, max] = line.split("-");
+        this.min = parseInt(min);
+        this.max = parseInt(max);
+    }
+}
 
+class Pair {
+    public elf1: ElfRange;
+    public elf2: ElfRange;
+
+    constructor(public line: string) {
+        let [e1, e2] = line.split(",");
+        this.elf1 = new ElfRange(e1);
+        this.elf2 = new ElfRange(e2);
+    }
+
+    public rangeIsContained(a: ElfRange, b: ElfRange): boolean {
+        return a.min >= b.min && a.max <= b.max;
+    }
+
+    public eitherRangeIsContained(): boolean {
+        return this.rangeIsContained(this.elf1, this.elf2) || this.rangeIsContained(this.elf2, this.elf1);
+    }
+
+    public pairsOverlap(): boolean {
+        return this.elf1.max >= this.elf2.min && this.elf2.max >= this.elf1.min;
+    }
+}
+
+let contents = readFile(`${ROOT_DIR}/input.txt`);
+let lines = contents.split("\n");
+let pairs = lines.map(l => new Pair(l));
+
+console.log("==== PART 1 ====");
+let contained = pairs.filter(p => p.eitherRangeIsContained());
+console.log(`Contained: ${contained.length}`);
 
 console.log("==== PART 2 ====");
-contents = readFile(`${ROOT_DIR}/input.txt`);
-
+let overlaps = pairs.filter(p => p.pairsOverlap());
+console.log(`Overlaps: ${overlaps.length}`);
