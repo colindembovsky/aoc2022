d822f254a38631b9e39f2e0eaedba3e03d0e9437
colindembovsky <colindembovsky@gmail.com>
2022-12-05 23:33:56 -0600

    Solved day6

--- day06/part1.ts
+++ day06/part1.ts
@@ -6,11 +6,25 @@ const ROOT_DIR="day06";
 function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
+let contents = readFile(`${ROOT_DIR}/input.txt`);
+let lines = contents.split("\n");
+
+function getPacketStart(line: string, conseq: number): number {
+    // find the first 4 char segment where all 4 chars are different
+    let i = 0;
+    while (i < line.length - conseq) {
+        let seg = line.substring(i, i + conseq);
+        let segSet = new Set(seg);
+        if (segSet.size === conseq) {
+            return i + conseq;
+        }
+        i++;
+    }
+    return -1;
+}
 
 console.log("==== PART 1 ====");
-let contents = readFile(`${ROOT_DIR}/test-input.txt`);
-
+lines.forEach(line => console.log(getPacketStart(line, 4)));
 
 console.log("==== PART 2 ====");
-contents = readFile(`${ROOT_DIR}/input.txt`);
-
+lines.forEach(line => console.log(getPacketStart(line, 14)));
