f393adc5a767b06f93d67d993e83e2ceaacb603a
colindembovsky <colindembovsky@gmail.com>
2022-12-10 15:08:45 -0600

    Solve 10.1

--- day10/part1.ts
+++ day10/part1.ts
@@ -7,12 +7,41 @@ function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
-let contents = readFile(`${ROOT_DIR}/easy-input.txt`);
+class Op {
+    constructor(public name: string, public val: number, public X: number) {}
+}
 
+let contents = readFile(`${ROOT_DIR}/input.txt`);
+let initialOperations = contents.split("\n");
 
+let operations: Op[] = [];
+let xAcc = 1;
+initialOperations.forEach(op => {
+    let [opName, opValue] = op.split(" ");
+    if (opName === "addx") {
+        operations.push(new Op("===", 0, xAcc));
+        let addVal = parseInt(opValue);
+        xAcc += addVal;
+        operations.push(new Op(opName, addVal, xAcc));
+    } else if (opName === "noop") {
+        operations.push(new Op(opName, 0, xAcc));
+    }
+});
 
 console.log("==== PART 1 ====");
+operations.forEach((op, i) => {
+    console.log(`Cycle ${i + 1}: ${op.name} ${op.val} (register X: ${op.X})`);
+});
+console.log("----");
 
+let cycles = [20, 60, 100, 140, 180, 220];
+let total = 0;
+cycles.forEach(cycle => {
+    let op = operations[cycle - 2];
+    console.log(`Cycle ${cycle}: ${op.name} ${op.val} (register X: ${op.X})`);
+    total += op.X * cycle;
+});
+console.log(`Total: ${total}`);
 
 console.log("==== PART 2 ====");
 contents = readFile(`${ROOT_DIR}/input.txt`);
