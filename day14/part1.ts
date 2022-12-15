832df9a767b886fb1b09bf94d529fb5e5c53b5c5
colindembovsky <colindembovsky@gmail.com>
2022-12-15 12:39:55 -0600

    Solve 14.2

--- day14/part1.ts
+++ day14/part1.ts
@@ -90,6 +90,20 @@ function parseLines(line: string) {
     return lines;
 }
 
+function printCave(start: Point, end: Point) {
+    for (let y = start.y; y <= end.y; y++) {
+        let line = "";
+        for (let x = start.x; x <= end.x; x++) {
+            let point = new Point(x, y);
+            if (caveMap.has(point.index)) {
+                line += caveMap.get(point.index);
+            } else {
+                line += ".";
+            }
+        }
+        console.log(line);
+    }
+}
 
 let contents = readFile(`${ROOT_DIR}/input.txt`);
 let lineStrings = contents.split("\n");
@@ -98,12 +112,13 @@ for (let line of lineStrings) {
     lines = lines.concat(parseLines(line));
 }
 let startPoint = new Point(500, 0);
+let fullPoints = [startPoint.downPoint(), startPoint.downLeftPoint(), startPoint.downRightPoint()];
 let highestY = lines.sort((a, b) => b.start.y - a.start.y)[0].start.y;
 
 console.log("==== PART 1 ====");
 let finished = false;
 let grainCount = -1;
-while(!finished) {
+while (!finished) {
     grainCount++;
     let grain = new Point(startPoint.x, startPoint.y);
     while (true) {
@@ -121,5 +136,25 @@ while(!finished) {
 }
 console.log(`Grain Count: ${grainCount}`);
 
-//console.log("==== PART 2 ====");
+console.log("==== PART 2 ====");
+let xSorted = lines.sort((a, b) => a.start.x - b.start.x);
+let floorLine = new Line(new Point(xSorted[0].start.x - 10000, highestY + 2), new Point(xSorted[lines.length - 1].start.x + 10000, highestY + 2));
+lines.push(floorLine);
 
+caveMap.clear();
+finished = false;
+grainCount = 1;
+while (!finished) {
+    grainCount++;
+    let grain = new Point(startPoint.x, startPoint.y);
+    while (true) {
+        let newPos = grain.nextPoint();
+        if (newPos === undefined) {
+            caveMap.set(grain.index, "o");
+            finished = fullPoints.every(p => caveMap.get(p.index) === "o");
+            break;
+        }
+        grain = newPos;
+    }
+}
+console.log(`Grain Count: ${grainCount}`);
