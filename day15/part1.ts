25d66ccc18e8b71e2c8310cc21ed14937b23477e
colindembovsky <colindembovsky@gmail.com>
2022-12-15 16:53:39 -0600

    Solve 15.1

--- day15/part1.ts
+++ day15/part1.ts
@@ -7,13 +7,87 @@ function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
-let contents = readFile(`${ROOT_DIR}/easy-input.txt`);
+class Point {
+    index: string;
+    constructor(public x: number, public y: number) {
+        this.index = `${x},${y}`;
+    }
 
+    manhattanDistance(other: Point): number {
+        return Math.abs(this.x - other.x) + Math.abs(this.y - other.y);
+    }
+}
 
+class Sensor {
+    location: Point;
+    closestBeacon: Point;
+    manhattanToClosestBeacon: number;
+
+    constructor(info: string) {
+        // parse regex for 'Sensor at x=2, y=18: closest beacon is at x=-2, y=15'
+        const regex = /Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/;
+        const match = regex.exec(info);
+        if (match) {
+            this.location = new Point(parseInt(match[1]), parseInt(match[2]));
+            this.closestBeacon = new Point(parseInt(match[3]), parseInt(match[4]));
+            this.manhattanToClosestBeacon = this.location.manhattanDistance(this.closestBeacon);
+        } else {
+            throw new Error(`Could not parse sensor info: ${info}`);
+        }
+    }
+
+    // get all the points that are within the manhattan distance from the sensor
+    getPointsWithinManhattanDistance(): Point[] {
+        const points: Point[] = [];
+        const startX = this.location.x - this.manhattanToClosestBeacon;
+        const endX = this.location.x + this.manhattanToClosestBeacon;
+        const startY = this.location.y - this.manhattanToClosestBeacon;
+        const endY = this.location.y + this.manhattanToClosestBeacon;
+        for (let x = startX; x <= endX; x++) {
+            for (let y = startY; y <= endY; y++) {
+                const point = new Point(x, y);
+                if (point.manhattanDistance(this.location) <= this.manhattanToClosestBeacon) {
+                    points.push(point);
+                }
+            }
+        }
+        return points;
+    }
+
+    pointsWithinManhattanDistanceOnLine(lineY: number): Point[] {
+        const points: Point[] = [];
+        const startX = this.location.x - this.manhattanToClosestBeacon;
+        const endX = this.location.x + this.manhattanToClosestBeacon;
+        for (let x = startX; x <= endX; x++) {
+            const point = new Point(x, lineY);
+            if (point.manhattanDistance(this.location) <= this.manhattanToClosestBeacon) {
+                points.push(point);
+            }
+        }
+        return points;
+    }
+}
+
+//let contents = readFile(`${ROOT_DIR}/easy-input.txt`);
+//let lineY = 10;
+
+let contents = readFile(`${ROOT_DIR}/input.txt`);
+let lineY = 2000000;
+console.log("be patient - this takes a while...");
+
+let sensors: Sensor[] = contents.split("\n").map(line => new Sensor(line));
 
 console.log("==== PART 1 ====");
+let manhattanPointsSet = new Set<string>();
+sensors.forEach(sensor => {
+    sensor.pointsWithinManhattanDistanceOnLine(lineY).forEach(point => manhattanPointsSet.add(point.index));
+});
+let beacons = new Set<string>();
+sensors.forEach(sensor => beacons.add(sensor.closestBeacon.index));
 
+// get the difference of sets
+let pointsNotBeacons = new Set([...manhattanPointsSet].filter(x => !beacons.has(x)));
+console.log([...pointsNotBeacons].length);
 
-console.log("==== PART 2 ====");
-contents = readFile(`${ROOT_DIR}/input.txt`);
+//console.log("==== PART 2 ====");
 
