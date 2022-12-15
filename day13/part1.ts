7108449641104825439dff6146cb46a381c44a47
colindembovsky <colindembovsky@gmail.com>
2022-12-15 10:15:50 -0600

    Solve 13.2

--- day13/part1.ts
+++ day13/part1.ts
@@ -7,46 +7,37 @@ function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
-function parseLine(line: string): number[] {
-    // read line into json array
-    let json = JSON.parse(line);
-    return json;
+function compareItems(left: any, right: any): number {
+    if (typeof left === "number" && typeof right === "number") {
+        if (left < right) return 1;
+        if (left > right) return -1;
+        return 0;
+    }
+    let leftArr = Array.isArray(left) ? left : [left];
+    let rightArr = Array.isArray(right) ? right : [right];
+    return compareArrays(leftArr, rightArr);
+}
+
+function compareArrays(leftArray: any[], rightArray: any[]): number {
+    let max = Math.max(leftArray.length, rightArray.length);
+    for (let i = 0; i < max; i++) {
+        let left = leftArray[i];
+        let right = rightArray[i];
+        
+        if (left === undefined) return 1;
+        if (right === undefined) return -1;
+        let result = compareItems(left, right);
+        if (result !== 0) {
+            return result;
+        }
+    }
+    return 0;
 }
 
 class SignalPair {
     isCorrectOrder: boolean = false;
-    constructor(public left: any[], public right: any[], public index: number) {
-        console.log(`Item ${index}`);
-        this.isCorrectOrder = this.compareArrays(this.left, this.right) > 0;
-        console.log(`Correct order: ${this.isCorrectOrder}`);
-        console.log("");
-    }
-
-    compareItems(left: any, right: any): number {
-        if (typeof left === "number" && typeof right === "number") {
-            if (left < right) return 1;
-            if (left > right) return -1;
-            return 0;
-        }
-        let leftArr = Array.isArray(left) ? left : [left];
-        let rightArr = Array.isArray(right) ? right : [right];
-        return this.compareArrays(leftArr, rightArr);
-    }
-
-    compareArrays(leftArray: any[], rightArray: any[]): number {
-        let max = Math.max(leftArray.length, rightArray.length);
-        for (let i = 0; i < max; i++) {
-            let left = leftArray[i];
-            let right = rightArray[i];
-            
-            if (left === undefined) return 1;
-            if (right === undefined) return -1;
-            let result = this.compareItems(left, right);
-            if (result !== 0) {
-                return result;
-            }
-        }
-        return 0;
+    constructor(public left: any[], public right: any[], public index: number = 0) {
+        this.isCorrectOrder = compareArrays(this.left, this.right) > 0;
     }
 }
 
@@ -59,6 +50,15 @@ let pairs = linePairs.map(p => {
 });
 
 console.log("==== PART 1 ====");
-console.log(`There are ${pairs.filter(p => p.isCorrectOrder).length} in correct order.`);
-console.log(`indexes of correct pairs: ${pairs.filter(p => p.isCorrectOrder).map(p => p.index).join(', ')}`);
 console.log(`Sum of correct pair indexes: ${pairs.filter(p => p.isCorrectOrder).reduce((a, b) => a + b.index, 0)}`);
+
+console.log("==== PART 2 ====");
+let allItems = contents.split("\n").filter(p => p.length > 0).map(p => JSON.parse(p));
+let item2 = [[2]];
+let item6 =  [[6]];
+allItems.push(item2);
+allItems.push(item6);
+allItems.sort((a, b) => compareItems(b, a));
+let index2 = allItems.indexOf(item2) + 1;
+let index6 = allItems.indexOf(item6) + 1;
+console.log(`Decoder key: ${index2 * index6}`);
