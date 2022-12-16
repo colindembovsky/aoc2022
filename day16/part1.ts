2b9065bcead94027a0247f7b16860fece6134a96
colindembovsky <colindembovsky@gmail.com>
2022-12-15 23:25:15 -0600

    Prep day16

---
+++ day16/part1.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+const ROOT_DIR="day16";
+
+// read in a file and print it out to the console
+function readFile(fileName: string): string {
+    return fs.readFileSync(fileName, "utf8");
+}
+
+let contents = readFile(`${ROOT_DIR}/easy-input.txt`);
+
+console.log("==== PART 1 ====");
+
+
+//console.log("==== PART 2 ====");
+//contents = readFile(`${ROOT_DIR}/input.txt`);
+
