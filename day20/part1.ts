8da77e1052df3b2c97ee49a26517bbda26ce2b4f
colindembovsky <colindembovsky@gmail.com>
2023-01-19 12:38:47 -0600

    Change to nodes - solved part 1

--- day20/part1.ts
+++ day20/part1.ts
@@ -7,48 +7,77 @@ function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
-let contents = readFile(`${ROOT_DIR}/easy-input.txt`);
+let contents = readFile(`${ROOT_DIR}/input.txt`);
 let lines = contents.split("\n");
 
-// make an array of numbers from the lines
-let originalNumbers = lines.map(line => parseInt(line));
-let numbers = originalNumbers.slice();
+class Node {
+    left: Node | undefined = undefined;
+    right: Node | undefined = undefined;
+    constructor(public value: number) {}
+}
 
 console.log("==== PART 1 ====");
 
-for(let i = 0; i < originalNumbers.length; i++) {
-    const num = originalNumbers[i];
-    if (num === 0) {
+// make an array of numbers from the lines
+let numbers = lines.map(line => new Node(parseInt(line)));
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].left  = numbers[(i - 1) % numbers.length];
+    numbers[i].right = numbers[(i + 1) % numbers.length];
+}
+numbers[0].left = numbers[numbers.length - 1];
+const cycle = numbers.length - 1;
+
+function printNums() {
+    let k = numbers[0];
+    let n = "";
+    for (let i = 0; i < numbers.length; i++) {
+        n += k.value + ", ";
+        k = k.right!;
+    }
+    console.log(n);
+}
+
+let zeroNode = undefined;
+
+for(let node of numbers) {
+    if (node.value === 0) {
+        zeroNode = node;
         continue;
     }
-    const j = numbers.indexOf(num);
+    console.log(`move k: ${node.value}`);
+    let target = node;
+    if (node.value > 0) {
+        for (let i = 0; i < node.value % cycle; i++) {
+            target = target.right!;
+        }
+    } else {
+        for (let i = 0; i < (-node.value + 1) % cycle; i++) {
+            target = target.left!;
+        }
+    }
 
-    let newIndex = j + num;
-    if (newIndex > numbers.length) {
-        newIndex = newIndex % numbers.length + 1;
+    if (node === target) {
+        continue;
     }
-    if (newIndex < 0 && -newIndex >= numbers.length) {
-        newIndex = newIndex % numbers.length - 1;
-    }
-    
-    numbers.splice(j, 1);
-    if (newIndex === 0) {
-        numbers.push(num);
-    } else if (newIndex === numbers.length) {
-        numbers.unshift(num);
-    }
-    else {
-        numbers.splice(newIndex, 0, num);
-    }
-    //console.log(`num: ${num}: ${numbers.join(",")}`);
+
+    // remove n from the list
+    node.right!.left = node.left;
+    node.left!.right = node.right;
+
+    // insert n after target
+    target.right!.left = node;
+    node.right = target.right;
+    target.right = node;
+    node.left = target;
 }
-let zeroIndex = numbers.indexOf(0);
-console.log(numbers.length);
-const num1 = numbers[(1000 + zeroIndex) % numbers.length];
-const num2 = numbers[(2000 + zeroIndex) % numbers.length];
-const num3 = numbers[(3000 + zeroIndex) % numbers.length];
-console.log(`Result is ${num1 + num2 + num3}`);
-//< 10667
-//!4601
+
+let coordinate = 0;
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 1000; j++) {
+        zeroNode = zeroNode!.right!;
+    }
+    coordinate += zeroNode!.value;
+}
+console.log(coordinate);
 
 console.log("==== PART 2 ====");
